4cd38734edf1cd22bb77ffa50170c639eb2c1009
Lucas Iorio <lucas.iorio@gmail.com>
2025-06-29 13:50:58 -0300

    Documentando produtos detalhados

--- src/main/java/br/com/byiorio/desafio/controllers/ProdutoDetalhesController.java
+++ src/main/java/br/com/byiorio/desafio/controllers/ProdutoDetalhesController.java
@@ -10,9 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.byiorio.desafio.models.ProdutoDetalhadoDTO;
 import br.com.byiorio.desafio.services.ProdutoDetalhadoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping(value = "/produtos-detalhe")
+@Tag(name = "Detalhe dos protudos", description = "Operações relacionadas aos detalhes dos produtos")
 public class ProdutoDetalhesController {
 
     ProdutoDetalhadoService produtoDetalhadoService;
@@ -21,14 +28,19 @@ public class ProdutoDetalhesController {
         this.produtoDetalhadoService = produtoDetalhadoService;
     }
 
+    @Operation(summary = "Buscar produto por ID", description = "Retorna os detalhes de um produto pelo seu ID.")
+    @ApiResponse(responseCode = "200", description = "Produto detalhado encontrado com sucesso")
+    @ApiResponse(responseCode = "404", description = "Produto não encontrado", content = @Content(schema = @Schema(implementation = br.com.byiorio.desafio.models.BasicErrorDTO.class)))
     @GetMapping("/{id}")
-    public ResponseEntity<ProdutoDetalhadoDTO> consultar(@PathVariable String id) {
+    public ResponseEntity<ProdutoDetalhadoDTO> consultar(
+            @Parameter(description = "ID do produto detalhado", example = "abc123") @PathVariable String id) {
         return ResponseEntity.ok(produtoDetalhadoService.buscar(id));
     }
 
+    @Operation(summary = "Listar todos os produtos detalhados", description = "Retorna uma lista com todos os produtos detalhados.")
+    @ApiResponse(responseCode = "200", description = "Lista de produtos detalhados retornada com sucesso")
     @GetMapping("/")
     public ResponseEntity<List<ProdutoDetalhadoDTO>> consultar() {
         return ResponseEntity.ok(produtoDetalhadoService.buscarTodos());
     }
-
 }
