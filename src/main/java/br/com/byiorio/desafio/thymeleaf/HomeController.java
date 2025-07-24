cf9837516100db11e5c3a3b67dd3df6bbf876130
Lucas Iorio <lucas.iorio@gmail.com>
2025-07-24 20:10:29 -0300

    Aprendendo nova tecnologia

---
+++ src/main/java/br/com/byiorio/desafio/thymeleaf/HomeController.java
@@ -0,0 +1,27 @@
+package br.com.byiorio.desafio.thymeleaf;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.byiorio.desafio.models.ProdutoDetalhadoResponse;
+import br.com.byiorio.desafio.services.ProdutoDetalhadoService;
+
+@Controller
+public class HomeController {
+
+    ProdutoDetalhadoService produtoDetalhadoService;
+
+    public HomeController(ProdutoDetalhadoService produtoDetalhadoService) {
+        this.produtoDetalhadoService = produtoDetalhadoService;
+    }
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        List<ProdutoDetalhadoResponse> listaProduto = produtoDetalhadoService.buscarTodos(null);
+        model.addAttribute("listaProdutos", listaProduto);
+        return "home";
+    }
+}
