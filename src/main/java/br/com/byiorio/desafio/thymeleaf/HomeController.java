7f7636bfd34a0d7852b4fb5ceb401207788bfe1e
Lucas Iorio <lucas.iorio@gmail.com>
2025-07-30 10:01:42 -0300

    Funcionando a primeira pagina

--- src/main/java/br/com/byiorio/desafio/thymeleaf/HomeController.java
+++ src/main/java/br/com/byiorio/desafio/thymeleaf/HomeController.java
@@ -5,14 +5,20 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.byiorio.desafio.models.ProdutoDetalhadoResponse;
 import br.com.byiorio.desafio.services.CategoriaService;
 import br.com.byiorio.desafio.services.ProdutoDetalhadoService;
+import br.com.byiorio.desafio.thymeleaf.dto.HomeRequest;
 
 @Controller
 public class HomeController {
+
+    private static final String LISTA_PRODUTOS = "listaProdutos";
+    private static final String CATEGORIAS = "categorias";
 
     ProdutoDetalhadoService produtoDetalhadoService;
     CategoriaService categoriaService;
@@ -26,22 +32,45 @@ public class HomeController {
     public String home(@RequestParam(required = false) String categoria, Model model) {
 
         List<ProdutoDetalhadoResponse> listaProduto = null;
-        if (categoria == null){
+        // Listando produtos
+        if (categoria == null) {
             listaProduto = produtoDetalhadoService.buscarTodos(null);
-        }else{
+        } else {
             listaProduto = produtoDetalhadoService.buscarTodos(categoria);
         }
+        model.addAttribute(LISTA_PRODUTOS, listaProduto);
 
-        model.addAttribute("listaProdutos", listaProduto);
+        model.addAttribute(CATEGORIAS, categoriaService.buscarTodos());
+        return "home";
+    }
 
-        model.addAttribute("categorias", categoriaService.buscarTodos());
+    @PostMapping("/home")
+    public String homePost(@ModelAttribute HomeRequest request, Model model) {
+        List<ProdutoDetalhadoResponse> listaProduto = null;
+
+        // Listando produtos
+        if (request.getCategoria() == null) {
+            listaProduto = produtoDetalhadoService.buscarTodos(null);
+        } else {
+            listaProduto = produtoDetalhadoService.buscarTodos(request.getCategoria());
+        }
+
+        // Filtrando busca
+        if (request.getBusca() != null) {
+            listaProduto = listaProduto.stream()
+                    .filter(p -> p.getProduto().getTitulo().toUpperCase().contains(request.getBusca().toUpperCase()))
+                    .toList();
+        }
+        model.addAttribute(LISTA_PRODUTOS, listaProduto);
+
+        model.addAttribute(CATEGORIAS, categoriaService.buscarTodos());
         return "home";
     }
 
     @GetMapping("/produto")
     public String produto(Model model) {
         List<ProdutoDetalhadoResponse> listaProduto = produtoDetalhadoService.buscarTodos(null);
-        model.addAttribute("listaProdutos", listaProduto);
+        model.addAttribute(LISTA_PRODUTOS, listaProduto);
         return "produto";
     }
 }
